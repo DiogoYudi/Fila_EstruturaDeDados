585598393c956ecea28808834045c476f97cd14e
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-04-20 16:49:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿const int MAX = 20;
+
+void Insere(int[] p, int na, ref int f)
+{
+    p[f] = na;
+    f++;
+}
+
+int Remove(int[] p, ref int i)
+{
+    i++;
+    return (p[i]);
+}
+
+void adicionar(int[] p, ref int i, ref int f)
+{
+    Console.Write("Digite o número do avião: ");
+    int na = Convert.ToInt32(Console.ReadLine());
+    Insere(p, na, ref f);
+}
+
+void consultar(int i, int f)
+{
+    Console.WriteLine(f - i);
+}
+
+void autorizar(int[] p, ref int i)
+{
+    Remove(p, ref i);
+}
+
+void apresentar(int[] p, int i, int f)
+{
+    while (i < f)
+    {
+        Console.WriteLine(p[i]);
+        i++;
+    }
+}
+
+void consultar1(int[] p, int i)
+{
+    Console.WriteLine(p[i]);
+}
+
+int[] p = new int[MAX];
+int i = 0, f = 0, op;
+
+int menu()
+{   Console.WriteLine("MENU");
+    Console.WriteLine("1. Adicionar um avião à fila");
+    Console.WriteLine("2. Consultar a quantidade de aviões");
+    Console.WriteLine("3. Autorizar a decolagem de avião");
+    Console.WriteLine("4. Apresentar os aviões presente na fila");
+    Console.WriteLine("5. Consultar o número do primeiro avião na fila");
+    Console.WriteLine("0. Sair");
+    Console.Write("Escolha uma opção: ");
+    int op = Convert.ToInt32(Console.ReadLine());
+    return op;
+}
+while ((op = menu()) != 0)
+{
+    switch (op)
+    {
+        case 1:
+            adicionar(p, ref i, ref f);
+            break;
+        case 2:
+            consultar(i, f);
+            break;
+        case 3:
+            autorizar(p, ref i);
+            break;
+        case 4:
+            apresentar(p, i, f);
+            break;
+        case 5:
+            consultar1(p, i);
+            break;
+    }
+
+}
